207215728f26c8ad62a111708bb54f7549fb3461
Esteban Vanegas <juanestebanvanegasvela@gmail.com>
2023-10-29 15:24:54 -0500

    Se modifico la estructura de javascript.

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 let countdown;
 const timerDisplay = document.querySelector(".countdown");
 const endDate = document.querySelector('input[type="date"]');
-const satrtButton = document.querySelector("#start-button");
+const startButton = document.querySelector("#start-button");
 
-satrtButton.addEventListener("click", function () {
-  const endTime = new Date(endDate.vale);
+startButton.addEventListener("click", function () {
+  const endTime = new Date(endDate.value);
   startTimer(endTime);
 });
 
 function startTimer(endTime) {
   clearInterval(countdown);
+
   const seconds = Math.round((endTime - Date.now()) / 1000);
+
   if (seconds < 0) {
     clearInterval(countdown);
     return;
   }
+
   displayTimeLeft(seconds);
+
   countdown = setInterval(() => {
     const secondsLeft = Math.round((endTime - Date.now()) / 1000);
+
     if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
+
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
 function displayTimeLeft(seconds) {
   const days = Math.floor(seconds / 86400);
-  const hours = Math.floor((seconds % 86400) / 36000);
+  const hours = Math.floor((seconds % 86400) / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const remainderSeconds = seconds % 60;
-  document.querrySelector("#days").textContent = `${
+
+  document.querySelector("#days").textContent = `${
     days < 10 ? "0" : ""
   }${days}`;
-  document.querrySelector("#hours").textContent = `${
+  document.querySelector("#hours").textContent = `${
     hours < 10 ? "0" : ""
   }${hours}`;
-  document.querrySelector("#minutes").textContent = `${
+  document.querySelector("#minutes").textContent = `${
     minutes < 10 ? "0" : ""
   }${minutes}`;
-  document.querrySelector("#seconds").textContent = `${
+  document.querySelector("#seconds").textContent = `${
     remainderSeconds < 10 ? "0" : ""
   }${remainderSeconds}`;
 }
